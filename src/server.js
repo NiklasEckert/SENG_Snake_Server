e236e4918e4d9df1fb5534c8e9487bb15f2462c9
NiklasEckert <niklas.eckert.1807@gmail.com>
2021-06-09 18:14:41 +0200

    Modified: create new player for every socket connecting

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
+const Player = require("./player");
 const httpServer = require("http").createServer()
 const io = require("socket.io")(httpServer, {
     origin: "*",
     methods: ["GET", "POST"],
     credentials: false
 })
-
 const PORT = Number(process.env.PORT) || 3000
+
+const players = []
 
 io.on("connection", socket => {
     console.log(`${socket.handshake.address} connected.`)
+    let player = new Player(socket, "name")
+    players.push(player)
+
+    socket.on("disconnect", () => {
+        if (players.find(p => p.socket === socket))
+            players.splice(players.findIndex(p => p.socket === socket), 1)
+
+        console.log(`${player.socket.handshake.address} / ${player.playerName} disconnecting.`)
+    })
+
+    console.log(players)
 })
 
 httpServer.listen(PORT, () => {
