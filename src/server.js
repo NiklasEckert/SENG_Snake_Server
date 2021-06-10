e267fede56ae4c70045df716224e3adb3f102e77
NiklasEckert <niklas.eckert.1807@gmail.com>
2021-06-10 18:08:56 +0200

    Modified: added event listener for the client:createLobby and client:joinLobby event

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const Player = require("./player");
+const Lobby = require("./lobby");
 const httpServer = require("http").createServer()
 const io = require("socket.io")(httpServer, {
     origin: "*",
@@ -8,11 +9,27 @@ const io = require("socket.io")(httpServer, {
 const PORT = Number(process.env.PORT) || 3000
 
 const players = []
+const lobbies = []
+let lobbyCount = 0;
 
 io.on("connection", socket => {
     console.log(`${socket.handshake.address} connected.`)
     let player = new Player(socket, "name")
     players.push(player)
+
+    socket.on("client:createLobby", options => {
+        lobbyCount++
+        const lobbyCode = generateLobbyCode()
+        let lobby = new Lobby(lobbyCode, options)
+        lobbies.push(lobby)
+        console.log(lobby)
+        socket.emit("server:lobbyCreated", lobbyCode)
+    })
+
+    socket.on("client:joinLobby", lobbyCode => {
+        let lobby = lobbies.find(lobby => lobby.code === lobbyCode)
+        lobby.joinLobby(player)
+    })
 
     socket.on("disconnect", () => {
         if (players.find(p => p.socket === socket))
@@ -20,9 +37,11 @@ io.on("connection", socket => {
 
         console.log(`${player.socket.handshake.address} / ${player.playerName} disconnecting.`)
     })
-
-    console.log(players)
 })
+
+function generateLobbyCode() {
+    return `lobby:${lobbyCount}`
+}
 
 httpServer.listen(PORT, () => {
     console.log(`Snake-Server is listening on port ${PORT}`)
