29dbca056f2dcf79cbe033c7d5b1bec6fbd622c5
NiklasEckert <niklas.eckert.1807@gmail.com>
2021-06-23 23:04:18 +0200

    Modified: Added an Snake reference to the player

--- src/player.js
+++ src/player.js
@@ -1,6 +1,24 @@
+const Direction = require("./Direction");
+
 module.exports = class Player {
+
+    #snake
+
     constructor(socket, playerName) {
         this.socket = socket
         this.playerName = playerName
+        this.direction = Direction.SOUTH
+
+        this.socket.on("client:changeDirection", direction => {
+            this.direction = direction
+        })
+    }
+
+    get snake() {
+        return this.#snake
+    }
+
+    set snake(snake) {
+        this.#snake = snake
     }
 }
