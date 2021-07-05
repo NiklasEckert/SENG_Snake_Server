d958ea2a2babd60d6acff5e8efcab30e621521db
NiklasEckert <niklas.eckert.1807@gmail.com>
2021-07-06 00:04:13 +0200

    Modified: Updated the documentation

--- src/GameState.js
+++ src/GameState.js
@@ -3,15 +3,18 @@
  */
 class GameState {
 
+    /** @member {Snake[]} - The snakes on the field */
+    snakes
+    /** @member {boolean} - Indicator if the game is still running. */
+    gameIsRunning
+
     /**
      * Create a GameState
      * @param snakes {Snake[]} - All snakes of the game.
      * @param gameIsRunning {boolean} - Set if the game is still running. (At least 2 players are still playing)
      */
     constructor(snakes, gameIsRunning) {
-        /** @member {Snake[]} - The snakes on the field */
         this.snakes = snakes
-        /** @member {boolean} - Indicator if the game is still running. (At least 2 players are still playing) */
         this.gameIsRunning = gameIsRunning
     }
 }
